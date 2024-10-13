939de367479afdd17554ab0b333e5664fd66e20a
Aaditya Panikath <aadityarajeev@gmail.com>
2024-10-13 17:53:06 +0530

    feat: time based tokyonight theme

--- init.lua
+++ init.lua
@@ -835,7 +835,12 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      local hour = tonumber(os.date '%H')
+      if hour >= 18 or hour < 7 then
+        vim.cmd.colorscheme 'tokyonight-night'
+      else
+        vim.cmd.colorscheme 'tokyonight-day'
+      end
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
