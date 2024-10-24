af3287ba3591717e89543110170c561c66801dc3
Aaditya Panikath <aadityarajeev@gmail.com>
2024-10-24 20:33:00 +0530

    feat: added catppuccin colorscheme

--- lua/community.lua
+++ lua/community.lua
@@ -4,8 +4,14 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.go" },
-  -- import/override with your plugins folder
+  'AstroNvim/astrocommunity',
+  -- Packs
+  { import = 'astrocommunity.pack.lua' },
+  { import = 'astrocommunity.pack.go' },
+
+  -- Terminal Integration
+  { import = 'astrocommunity.terminal-integration.vim-tmux-navigator' },
+
+  -- Color Scheme
+  { import = 'astrocommunity.colorscheme.catppuccin' },
 }
